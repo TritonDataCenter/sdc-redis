d426c891feeb14338128e707dbbc3203eb8341dc
Josh Wilsdon <jwilsdon@joyent.com>
2012-02-13 21:42:53 -0800

    HEAD-946 setup redis for building in MG

---
+++ package.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/bash
+
+set -o xtrace
+set -o errexit
+
+RELEASE_TARBALL=$1
+echo "Building ${RELEASE_TARBALL}"
+
+ROOT=$(pwd)
+
+tmpdir="/tmp/redis.$$"
+mkdir -p ${tmpdir}/root/var/svc
+mkdir -p ${tmpdir}/root/var/tmp
+mkdir -p ${tmpdir}/site
+
+cp ${ROOT}/redis.smf ${tmpdir}/root/var/tmp
+cp ${ROOT}/redis-2.4.1.tar.bz2 ${tmpdir}/root/var/svc
+
+(cd ${tmpdir}; tar -jcf ${ROOT}/${RELEASE_TARBALL} root site)
+
+rm -rf ${tmpdir}
